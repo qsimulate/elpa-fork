408b30a72a44237f00151ce860d6f3bbcb32a88f
Toru Shiozaki <shiozaki.toru@gmail.com>
2020-02-10 18:16:50 -0500

    Update elpa.hh
    extern "C"

--- elpa_hh/elpa.hh
+++ elpa_hh/elpa.hh
@@ -6,18 +6,21 @@
 
 #include <elpa_hh/elpa_version.hh>
 
-struct elpa_struct;
-typedef struct elpa_struct *elpa_t;
+extern "C" {
+  struct elpa_struct;
+  typedef struct elpa_struct *elpa_t;
 
-struct elpa_autotune_struct;
-typedef struct elpa_autotune_struct *elpa_autotune_t;
-
+  struct elpa_autotune_struct;
+  typedef struct elpa_autotune_struct *elpa_autotune_t;
+}
 
 #include <elpa_hh/elpa_constants.hh>
 #include <elpa_hh/elpa_generated_c_api.hh>
 #include <elpa_hh/elpa_generated.hh>
 #include <elpa_hh/elpa_generic.hh> // C++ template 
 
-const char *elpa_strerr(int elpa_error);
+extern "C" {
+  const char *elpa_strerr(int elpa_error);
+}
 
 #endif
