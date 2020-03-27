a9f30f2e20374908e039fdd11602457d1c378af5
Toru Shiozaki <shiozaki@qsimulate.com>
2020-03-27 01:46:44 -0400

    Revert "use python3 in autogen.sh instead of default python to avoid installation problems when multiple python binaries exist"
    This reverts commit 6d6e3549af25e8c652f1f4a7ddf8e150ba953b4c.

--- autogen.sh
+++ autogen.sh
@@ -5,5 +5,5 @@ mkdir -p m4/
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
-python3 $srcdir/generate_automake_test_programs.py > $srcdir/test_programs.am
+$srcdir/generate_automake_test_programs.py > $srcdir/test_programs.am
 autoreconf --force --install --verbose "$srcdir"
